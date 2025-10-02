32df786ffb2210ab535c2001d65ea4dda09790dd
jmez27 <jmesloh27@gmail.com>
2025-10-02 18:21:36 -0400

    Added Blue meep meep bot and a blue run.

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MyClass.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MyClass.java
@@ -3,11 +3,15 @@ package com.example.meepmeeptesting;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MyClass
 {
+    static int isRed = -1;
+    static int isRedRotate = 180;
+
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
@@ -16,13 +20,20 @@ public class MyClass
             .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
             .build();
 
-        //myBot = zoeRout1(meepMeep);
-        myBot = zoeRout2(meepMeep);
+        RoadRunnerBotEntity myBot2 = new DefaultBotBuilder(meepMeep)
+            // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+            .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+            .build();
+
+        myBot = zoeRout1(meepMeep);
+        myBot2 = zoeRout1Blue(meepMeep);
+        //myBot = zoeRout2(meepMeep);
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_JUICE_LIGHT)
             .setDarkMode(true)
             .setBackgroundAlpha(0.95f)
             .addEntity(myBot)
+            .addEntity(myBot2)
             .start();
     }
 
@@ -33,9 +44,9 @@ public class MyClass
             .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
             .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(60, 24,Math.toRadians(180) ))
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(60,  24, Math.toRadians(180) ))
             .waitSeconds(3)
-            .splineTo(new Vector2d(-12, 20), Math.toRadians(135))
+            .splineTo(new Vector2d(-12,  20), Math.toRadians(135))
             .waitSeconds(3)
             //Shoots first three balls.
             .turnTo(Math.toRadians(90))
@@ -48,6 +59,35 @@ public class MyClass
             .splineTo(new Vector2d(-12, 20), Math.toRadians(135+180))
             .waitSeconds(3)
             .splineTo(new Vector2d(36, 45), Math.toRadians(90))
+            .waitSeconds(3)
+            .build());
+
+        return myBot;
+    }
+
+    static public RoadRunnerBotEntity zoeRout1Blue(MeepMeep meepMeep)
+    {
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+            // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+            .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+            .setColorScheme(new ColorSchemeBlueDark())
+            .build();
+
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(60, -24, Math.toRadians(180) ))
+            .waitSeconds(3)
+            .splineTo(new Vector2d(-12, -20), Math.toRadians(135+90))
+            .waitSeconds(3)
+            //Shoots first three balls.
+            .turnTo(Math.toRadians(90+180))
+            .splineTo(new Vector2d(-12, -45), Math.toRadians(90+180))
+            .setReversed(true)
+            .splineTo(new Vector2d(-12, -20), Math.toRadians(135-90))
+            .waitSeconds(3)
+            .splineTo(new Vector2d(12, -45), Math.toRadians(90+180))
+            .setReversed(true)
+            .splineTo(new Vector2d(-12, -20), Math.toRadians(135-90))
+            .waitSeconds(3)
+            .splineTo(new Vector2d(36, -45), Math.toRadians(90+180))
             .waitSeconds(3)
             .build());
 
