41c34686bd7e58fe5c9ba5b05dbca5401b3e7e0f
InnovatorsRobotics <innovators3311@gmail.com>
2025-10-25 11:08:46 -0400

    Fieldcentric now works
    Fieldcentric now works.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/CentricCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/CentricCommand.java
@@ -5,7 +5,9 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.Drivebase.Centric.CentricDrive;
 import org.firstinspires.ftc.teamcode.Drivebase.Centric.TurnToHeading;
-import org.firstinspires.ftc.teamcode.FeedbackSystems.IMU.IMU;
+//import org.firstinspires.ftc.teamcode.FeedbackSystems.IMU.IMU;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.IMU;
 import org.firstinspires.ftc.teamcode.FeedbackSystems.PID.PIDController;
 import org.firstinspires.ftc.teamcode.Drivebase.MecanumDrive;
 
@@ -23,7 +25,13 @@ public class CentricCommand extends OpMode
     public void init()
     {
         drive = new MecanumDrive(hardwareMap, null);
-        imu = new IMU(hardwareMap, telemetry);
+        imu = hardwareMap.get(IMU.class, "imu");
+        RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(
+            RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
+            RevHubOrientationOnRobot.UsbFacingDirection.UP
+        );
+        imu.initialize(new IMU.Parameters(orientationOnRobot));
+        imu.resetYaw();
         turnToHeading = new TurnToHeading(telemetry, drive, imu);
         centricDrive = new CentricDrive(drive, telemetry);
     }
@@ -31,6 +39,17 @@ public class CentricCommand extends OpMode
     @Override
     public void loop()
     {
-        centricDrive.drive(gamepad1.left_stick_x, gamepad1.left_stick_y, imu.getAngle(), turnToHeading.turnToHeading(gamepad1.right_stick_x, gamepad1.right_stick_y, 0.2, 0.2),  gamepad1.right_trigger);
+        if (gamepad1.dpad_up)
+        {
+            imu.resetYaw();
+        }
+        centricDrive.drive(
+                gamepad1.left_stick_x,
+                gamepad1.left_stick_y,
+                imu.getRobotYawPitchRollAngles().getYaw(),
+//                turnToHeading.turnToHeading(gamepad1.right_stick_x, gamepad1.right_stick_y, 0.2, 0.2),
+                gamepad1.right_trigger,
+                gamepad1.right_stick_x
+        );
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/CentricCommandBetterVersion.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/CentricCommandBetterVersion.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.OpModes.TeleOp;
 
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -7,7 +8,9 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.teamcode.Drivebase.Centric.CentricDriveTheBetterVersion;
 import org.firstinspires.ftc.teamcode.Drivebase.Centric.TurnToHeading;
-import org.firstinspires.ftc.teamcode.FeedbackSystems.IMU.IMU;
+//import org.firstinspires.ftc.teamcode.FeedbackSystems.IMU.IMU;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.IMU;
 import org.firstinspires.ftc.teamcode.Drivebase.MecanumDrive;
 
 @TeleOp(name = "Centric Command", group = "FieldCentric")
@@ -23,7 +26,13 @@ public class CentricCommandBetterVersion extends OpMode
     public void init()
     {
         drive = new MecanumDrive(hardwareMap, null);
-        imu = new IMU(hardwareMap, telemetry);
+        imu = hardwareMap.get(com.qualcomm.robotcore.hardware.IMU.class, "imu");
+        RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
+                RevHubOrientationOnRobot.UsbFacingDirection.UP
+        );
+        imu.initialize(new IMU.Parameters(orientationOnRobot));
+        drive = new MecanumDrive(hardwareMap, null);
         turnToHeading = new TurnToHeading(telemetry, drive, imu);
         centricDriveTheBetterVersion = new CentricDriveTheBetterVersion(drive, telemetry, new ElapsedTime());
     }
@@ -31,7 +40,13 @@ public class CentricCommandBetterVersion extends OpMode
     @Override
     public void loop()
     {
-        centricDriveTheBetterVersion.drive(gamepad1.left_stick_x, gamepad1.left_stick_y, imu.getAngle(), turnToHeading.turnToHeading(gamepad1.right_stick_x, gamepad1.right_stick_y, 0.2, 0.2), new Gamepad());
-
+        centricDriveTheBetterVersion.drive(
+                gamepad1.left_stick_x,
+                gamepad1.left_stick_y,
+                imu.getRobotYawPitchRollAngles().getYaw(),
+//                turnToHeading.turnToHeading(gamepad1.right_stick_x, gamepad1.right_stick_y, 0.2, 0.2),
+                gamepad1.right_stick_x,
+                new Gamepad()
+        );
     }
 }
