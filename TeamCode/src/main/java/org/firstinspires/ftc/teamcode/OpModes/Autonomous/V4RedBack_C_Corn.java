c1d601f213013d7d86bfc2f8c6521707fa018167
jmez27 <jmesloh27@gmail.com>
2026-02-28 12:38:21 -0500

    Red Back C Corn still is short on time (around 4 seconds) to make the last volley of shots (if we want to pull from corner a second time). We still need to get manual turret offset working (or fix the turret tracking code), but launch velocity seems to be good. We still need to get more accuracy data on Corner still since our intake is still not pulling all 3 balls.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/V4RedBack_C_Corn.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/V4RedBack_C_Corn.java
@@ -108,7 +108,7 @@ public class V4RedBack_C_Corn extends LinearOpMode
 
 
                 TrajectoryActionBuilder yellow_drop = drive.actionBuilder(beginPose)
-                    .afterTime(0, actionsBackpack.turretTracking(drive,6))
+                    .afterTime(0, actionsBackpack.turretTracking(drive,5))
                     .afterTime(0, actionsBackpack.shootBallManual(9, 5, 1430, 30, drive)) //at launch zone
                     .splineTo(new Vector2d(50, 15), Math.toRadians(180), new TranslationalVelConstraint(10))
 //                    .afterTime(0, actionsBackpack.turretTracking(drive))
@@ -122,12 +122,18 @@ public class V4RedBack_C_Corn extends LinearOpMode
                     .waitSeconds(3)
 
                     .afterTime(0, actionsBackpack.intakeBall(-1))
-                    //.turnTo(90)
                     .strafeToLinearHeading(new Vector2d(65, 75), Math.toRadians(85), new TranslationalVelConstraint(40))
                     .waitSeconds(0.5)
                     .strafeToLinearHeading(new Vector2d(50, 15), Math.toRadians(90), new TranslationalVelConstraint(40))
-                    .afterTime(0, actionsBackpack.shootBallManual(9, 6, 1450, 35, drive))
-                    .waitSeconds(5)
+                    .afterTime(0, actionsBackpack.shootBallManual(9, 6, 1450, 35, drive)) //off to the left
+                    .waitSeconds(4)
+
+                    .afterTime(0, actionsBackpack.intakeBall(-1))
+                    .strafeToLinearHeading(new Vector2d(65, 75), Math.toRadians(85), new TranslationalVelConstraint(40))
+                    .waitSeconds(0.5)
+                    .strafeToLinearHeading(new Vector2d(50, 15), Math.toRadians(90), new TranslationalVelConstraint(40))
+                    .afterTime(0, actionsBackpack.shootBallManual(9, 6, 1450, 35, drive))  //runs out of time
+                    .waitSeconds(4)
                     .strafeToLinearHeading(new Vector2d(30, 15), Math.toRadians(90), new TranslationalVelConstraint(40))
 
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/V4RedFront_Gate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/V4RedFront_Gate.java
@@ -100,8 +100,9 @@ public class V4RedFront_Gate extends LinearOpMode
 
                 TrajectoryActionBuilder yellow_drop = drive.actionBuilder(beginPose)
 
+                    .afterTime(0, actionsBackpack.turretTracking(drive,4))
                     .strafeToLinearHeading(new Vector2d(-10, 12), Math.toRadians(90), new TranslationalVelConstraint(40))
-                    .afterTime(0, actionsBackpack.shootBallManual(9, 3, 1200, 20, drive))
+                    .afterTime(0, actionsBackpack.shootBallManual(9, 3, 1150, 20, drive))
                     .waitSeconds(6)
 
                     .afterTime(0, actionsBackpack.intakeBall(-1))
@@ -110,13 +111,13 @@ public class V4RedFront_Gate extends LinearOpMode
                     .strafeTo(new Vector2d(12, 45))
                     .strafeToLinearHeading(new Vector2d(4, 57), Math.toRadians(90)) //Gate
                     .strafeToLinearHeading(new Vector2d(-5, 10), Math.toRadians(90), new TranslationalVelConstraint(40))
-                    .afterTime(0, actionsBackpack.shootBallManual(9, 3, 1200, 20, drive))
+                    .afterTime(0, actionsBackpack.shootBallManual(9, 3, 1150, 20, drive))
                     .waitSeconds(3)
 
                     .afterTime(0, actionsBackpack.intakeBall(-1))
                     .strafeToLinearHeading(new Vector2d(-10, 56), Math.toRadians(90), new TranslationalVelConstraint(40)) //A
                     .strafeToLinearHeading(new Vector2d(-10, 12), Math.toRadians(90), new TranslationalVelConstraint(40))
-                    .afterTime(0, actionsBackpack.shootBallManual(9, 4, 1200, 20, drive))
+                    .afterTime(0, actionsBackpack.shootBallManual(9, 4, 1150, 20, drive))
                     .waitSeconds(3)
 
                     .strafeToLinearHeading(new Vector2d(10, 10), Math.toRadians(90))
