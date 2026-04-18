e43fbe1dc8624c8d737008c57f08bed575157b58
jmez27 <jmesloh27@gmail.com>
2026-04-18 09:25:38 -0400

    fix breaking on drift bla bla... no one will ever read this...

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivebase/Centric/CentricDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivebase/Centric/CentricDrive.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.Drivebase.Centric;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.Drivebase.MecanumDrive;
 
@@ -18,6 +20,10 @@ public class CentricDrive
 
     public void drive(double x,double y, double robot_heading, double slowMo, double turn)
     {
+        driveController.leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        driveController.rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        driveController.leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        driveController.rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         double speed = 8*Math.pow((slowMo-0.543879), 4) + 0.3;
 
         double drive_y = y * Math.cos(Math.toRadians(robot_heading)) + x * Math.sin(Math.toRadians(robot_heading));
@@ -30,9 +36,9 @@ public class CentricDrive
         }
         drive_x = drive_x * scalar;
         drive_y = drive_y * scalar;
-        if(drive_y == 0){
-            drive_y = 0.0001;
-        }
+//        if(drive_y == 0){
+//            drive_y = 0.0001;
+//        }
         driveController.driveMotors(-drive_y, turn, drive_x, speed);
         telemetry.update();
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/Version4_2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/Version4_2.java
@@ -5,6 +5,7 @@ import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.PoseVelocity2d;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.IMU;
@@ -79,13 +80,13 @@ public class Version4_2 extends LinearOpMode
 
         drive = new MecanumDrive(hardwareMap, null);
 
-//        imu = hardwareMap.get(IMU.class, "imu");
-//        RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(
-//                RevHubOrientationOnRobot.LogoFacingDirection.RIGHT,
-//                RevHubOrientationOnRobot.UsbFacingDirection.UP
-//        );
-//        imu.initialize(new IMU.Parameters(orientationOnRobot));
-//        imu.resetYaw();
+        imu = hardwareMap.get(IMU.class, "imu");
+        RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.RIGHT,
+                RevHubOrientationOnRobot.UsbFacingDirection.UP
+        );
+        imu.initialize(new IMU.Parameters(orientationOnRobot));
+        imu.resetYaw();
 
         turnToHeading = new TurnToHeading(telemetry, drive, imu);
         centricDrive = new CentricDrive(drive, telemetry);
@@ -128,33 +129,38 @@ public class Version4_2 extends LinearOpMode
             }
 
             // Drive code
-            driveController.gamepadController(gamepad1);
-//            if (gamepad1.dpad_left)
-//            {
-//                imu.resetYaw();
-//            }
-//            centricDrive.drive(
-//                    gamepad1.left_stick_x,
-//                    gamepad1.left_stick_y,
-//                    imu.getRobotYawPitchRollAngles().getYaw(),
-////                turnToHeading.turnToHeading(gamepad1.right_stick_x, gamepad1.right_stick_y, 0.2, 0.2),
-//                    gamepad1.left_trigger,
-//                    gamepad1.right_stick_x
-//            );
+            if(drive_mode%2 == 1)
+            {
+                driveController.gamepadController(gamepad1);
+            }
+            else
+            {
+                if (gamepad1.dpad_up)
+                {
+                    imu.resetYaw();
+                }
+                centricDrive.drive(
+                    gamepad1.left_stick_x,
+                    gamepad1.left_stick_y,
+                    imu.getRobotYawPitchRollAngles().getYaw(),
+//                turnToHeading.turnToHeading(gamepad1.right_stick_x, gamepad1.right_stick_y, 0.2, 0.2),
+                    gamepad2.left_trigger,
+                    gamepad1.right_stick_x
+                );
+            }
 
-
-            if (gamepad1.a && gamepad1.b && gamepad1.y && gamepad1.x && drive_mode_flag <= time.seconds())
+            if (gamepad1.back && drive_mode_flag <= time.seconds())
             {
                 drive_mode_flag += time.seconds() + 0.25;
                 drive_mode += 1;
             }
 
             // Color sensor and intake
-            if (gamepad1.right_trigger > 0.1 || gamepad2.y || gamepad2.right_trigger > 0.1 || gamepad2.left_trigger > 0.1)
+            if (gamepad1.right_trigger > 0.1 || gamepad1.y)
             {
                 intake.setPower(-1);
             }
-            else if (gamepad1.b)
+            else if (gamepad1.left_trigger > 0.1 )
             {
                 intake.setPower(1);
             }
@@ -163,7 +169,7 @@ public class Version4_2 extends LinearOpMode
                 intake.setPower(0);
             }
 
-            if (gamepad2.y || gamepad2.left_trigger > 0.1)
+            if (gamepad1.y)
             {
                 trigger.setPower(1);
             }
@@ -173,7 +179,7 @@ public class Version4_2 extends LinearOpMode
             }
 
             // Roadrunner positioning update
-            if ((aprilTagLocalizer.getDetectionID() == 20 || aprilTagLocalizer.getDetectionID() == 24))
+            if ((aprilTagLocalizer.getDetectionID() == 20 || aprilTagLocalizer.getDetectionID() == 24) && (gamepad1.start))
             {
 //                drive.localizer.setPose(new Pose2d(0,0,imu.getRobotYawPitchRollAngles().getYaw()));
                 drive.localizer.setPose(aprilTagLocalizer.getFieldPose());
@@ -191,30 +197,30 @@ public class Version4_2 extends LinearOpMode
 
             // Turret code
             double aiTurretHeading = 0;
-            if (gamepad2.b && !gamepad2.start)
+            if (gamepad1.b && !gamepad2.start)
             {
                 state = Version3.TurretState.resetting;
             }
-            else if (gamepad2.x)
+            else if (gamepad1.x)
             {
                 state = Version3.TurretState.tracking;
             }
-            else if (gamepad2.a && !gamepad2.start)
+            else if (gamepad1.a && !gamepad2.start)
             {
                 state = Version3.TurretState.stopped;
             }
 
-            if (gamepad2.left_bumper)
+            if (gamepad1.left_bumper)
             {
                 turretOffset -= 1;
                 //flag = time.seconds() + 0.3;
             }
-            else if (gamepad2.right_bumper)
+            else if (gamepad1.right_bumper)
             {
                 turretOffset += 1;
                 //flag = time.seconds() + 0.3;
             }
-            else if (gamepad2.dpad_up)
+            else if (gamepad1.dpad_down)
             {
                 turretOffset = 90;
             }
@@ -222,7 +228,7 @@ public class Version4_2 extends LinearOpMode
             switch (state)
             {
                 case resetting:
-                    turret.zero(gamepad2);
+                    turret.zero(gamepad1);
                     break;
                 case tracking:
 //                    aiTurretHeading = turret.turretAngleToFixedTarget(pose.position.x, pose.position.y, Math.toDegrees(pose.heading.toDouble()), velocity2d, shooterParameters[2], team, turretOffset);
@@ -242,6 +248,7 @@ public class Version4_2 extends LinearOpMode
 //            telemetry.addData("07_Robot linear velocity Y", velocity2d.linearVel.y);
 //            telemetry.addData("08_Robot angular velocity", velocity2d.angVel);
             telemetry.addData("09_Robot Heading", Math.toDegrees(pose.heading.toDouble()));
+            telemetry.addData("10_Drive Mode", drive_mode%2);
 
             telemetry.update();
 
